ecf072f7f1e3fed05d71688f91051b93a1e9b04f
Ashton <10988492+ashco@users.noreply.github.com>
2020-10-08 20:11:53 -0700

    init commit. Getting them commit squares

---
+++ 2020-10/08.py
@@ -0,0 +1,16 @@
+﻿class Solution:
+    def search(self, nums, target):
+        lo, hi = 0, len(nums) - 1
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
+
+print(Solution().search([-1,0,3,5,9,12], 9))
