3a3481049c96c7cea9cbc4fd8fca3d13daf269b2
Ashton <10988492+ashco@users.noreply.github.com>
2020-10-14 08:55:06 -0700

    Leetcode october 14 challenge

---
+++ 2020-10/14.py
@@ -0,0 +1,23 @@
+﻿def rob(nums):
+    def house_robber(nums):
+        rob = not_rob = 0
+
+        for num in nums:
+            rob, not_rob = num + not_rob, max(rob, not_rob)
+
+        return max(rob, not_rob)
+
+    if not nums:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+    else:
+        return max(house_robber(nums[1:]), house_robber(nums[:-1]))
+
+
+
+print(rob([2, 3, 2])) # 3
+print(rob([1, 2, 3, 1])) # 4
+print(rob([1])) # 0
+print(rob([1, 2, 4, 3, 3, 4, 6, 2, 2, 7, 1])) # 20?
+              #  x     x     x        x
